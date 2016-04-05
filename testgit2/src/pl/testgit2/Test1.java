d881a593b99db8d0d495caef4df9b0afa9237000
a.majkowski <a.majkowski@probit.com.pl>
2016-04-05 17:21:15 +0200

    work hard feature

--- testgit2/src/pl/testgit2/Test1.java
+++ testgit2/src/pl/testgit2/Test1.java
@@ -5,6 +5,13 @@ public class Test1 {
 	public static void main(String[] args) {
 		Test1 t = new Test1();
 		t.fru();
+		t.workHard();
+	}
+
+	private void workHard() {
+		for(int i=0; i<6; i++) {
+			System.out.println(i);
+		}
 	}
 
 	private void fru() {
