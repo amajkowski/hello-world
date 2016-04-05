bfc9db2711b1d58082a9e2c01183aeaae3f53b2f
a.majkowski <a.majkowski@probit.com.pl>
2016-04-05 17:15:43 +0200

    Test1 project - initial commit

---
+++ testgit2/src/pl/testgit2/Test1.java
@@ -0,0 +1,14 @@
+package pl.testgit2;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		Test1 t = new Test1();
+		t.fru();
+	}
+
+	private void fru() {
+		System.out.println("fru");
+	}
+
+}
